00af14c1b214c01c4c811e6c5b2460d88c2ff45c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-03 10:23:55 +0000

    Atualização

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -63,11 +63,16 @@ class _HomePageWidgetState extends State<HomePageWidget> {
           centerTitle: false,
           elevation: 2.0,
         ),
-        body: const SafeArea(
+        body: SafeArea(
           top: true,
           child: Column(
             mainAxisSize: MainAxisSize.max,
-            children: [],
+            children: [
+              Text(
+                'Meu primeiro texto',
+                style: FlutterFlowTheme.of(context).bodyMedium,
+              ),
+            ],
           ),
         ),
       ),
